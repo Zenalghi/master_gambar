7ae227e8a30590c335fcc56ef7e1cedd57fbb7ec
Zenalghi <fatih.rafah17@gmail.com>
2025-11-20 16:24:14 +0700

    MODEL imag status

--- lib/admin/master/models/image_status.dart
+++ lib/admin/master/models/image_status.dart
@@ -1,18 +1,34 @@
-import 'package:master_gambar/admin/master/models/g_gambar_utama.dart';
+// File: lib/admin/master/models/image_status.dart
+
 import 'package:master_gambar/admin/master/models/varian_body.dart';
 
 class ImageStatus {
   final VarianBody varianBody;
-  final GGambarUtama? gambarUtama;
+  final DateTime? gambarUtamaUpdatedAt;
+  final String? deskripsiOptional;
+  final bool hasGambarUtama;
 
-  ImageStatus({required this.varianBody, this.gambarUtama});
+  ImageStatus({
+    required this.varianBody,
+    this.gambarUtamaUpdatedAt,
+    this.deskripsiOptional,
+    required this.hasGambarUtama,
+  });
 
   factory ImageStatus.fromJson(Map<String, dynamic> json) {
     return ImageStatus(
+      // 1. Parse data Varian Body (dan induknya) menggunakan factory standar
       varianBody: VarianBody.fromJson(json),
-      gambarUtama: json['gambar_utama'] != null
-          ? GGambarUtama.fromJson(json['gambar_utama'])
+
+      // 2. Ambil data tambahan yang di-select khusus di controller
+      gambarUtamaUpdatedAt: json['gambar_utama_updated_at'] != null
+          ? DateTime.parse(json['gambar_utama_updated_at'])
           : null,
+
+      deskripsiOptional: json['deskripsi_optional'],
+
+      // 3. Cek keberadaan gambar utama dari relasi yang di-load
+      hasGambarUtama: json['gambar_utama'] != null,
     );
   }
 }
